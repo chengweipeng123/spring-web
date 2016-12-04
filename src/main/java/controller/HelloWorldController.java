adf3ab944dce5ac50d8850ac827aef95438c3955
ChengSeas <chengseas@SeasMBP.local>
2016-12-04 19:34:12 +0800

    SpringMVC 中响应 AJAX 请求

--- src/main/java/controller/HelloWorldController.java
+++ src/main/java/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import domain.Address;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by chengseas on 2016/12/3.
@@ -23,5 +26,21 @@ public class HelloWorldController {
         // 当浏览器里访问 http://localhost:8080/helloworld-freemarker 时，
         // 访问的是 /WEB-INF/view/ftl/helloworld-freemarker.htm
         return "helloworld-freemarker.htm";
+    }
+
+    @RequestMapping("/ajax")
+    @ResponseBody // 处理 AJAX 请求，返回响应的内容，而不是 View Name，需要在方法的前面加上 @ResponseBody。
+    public String handleAjax(){
+        return "{username:\"seas\",password:\"123456\"}";
+    }
+
+    //不直接返回 Json 字符串，而是返回一个对象，SpringMVC 自动的把对象转换为 Json，需要 Jackson。
+    @RequestMapping(value = "/json",method = RequestMethod.GET)
+    @ResponseBody
+    public Address getAddress(){
+        Address address = new Address();
+        address.setId(1);
+        address.setStreet("桃园");
+        return address;
     }
 }
---
+++ src/main/java/domain/Address.java
@@ -0,0 +1,26 @@
+package domain;
+
+/**
+ * Created by chengseas on 2016/12/4.
+ */
+public class Address {
+    private int id;
+    private String street;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
